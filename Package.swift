87df3c44994610744642dc1ab56e6b174e07c6c0
Denis Goldberg <goldbergdenis00@gmail.com>
2022-04-21 15:44:22 +0200

    Initial files.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CodableValue",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -19,10 +22,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CodableValue",
-            dependencies: []),
+            name: "CodableValue"
+            ),
         .testTarget(
             name: "CodableValueTests",
-            dependencies: ["CodableValue"]),
+            dependencies: ["CodableValue"]
+        )
+    ],
+    swiftLanguageVersions: [
+        .version("5.1")
     ]
 )
