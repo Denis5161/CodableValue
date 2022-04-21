413059bbcc35bdfff17c70e89e2e84150845fd91
Denis Goldberg <goldbergdenis00@gmail.com>
2022-04-21 16:47:18 +0200

    Add small change to UIImage.swift

--- Sources/CodableValue/UIImage+Ext.swift
+++ Sources/CodableValue/UIImage+Ext.swift
@@ -9,7 +9,6 @@ import UIKit
 
 extension UIImage: Encodable {
     public func encode(to encoder: Encoder) throws {
-        
         if let imageData = pngData() {
             var container = encoder.singleValueContainer()
             try container.encode(imageData)
