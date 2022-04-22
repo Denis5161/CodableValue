67e3e85f61583ace09cef3f973e6ca7e07819095
Denis Goldberg <goldbergdenis00@gmail.com>
2022-04-23 00:50:02 +0200

    Update NSImage+Ext.swift

--- Sources/CodableValue/AppKit/NSImage+Ext.swift
+++ Sources/CodableValue/AppKit/NSImage+Ext.swift
@@ -9,31 +9,34 @@
 
 import AppKit
 
-extension NSImage: CodableValueSupported {
-    public static let type = SupportedCodableTypes.image
+extension NSImage {
     
     ///Determines if NSImage encodes to JPEG. When set to `.png`, encodes a png file.
     ///
     ///- Note: This changes the encoded data for every UIImage.
-    public static var encodedFileType: NSBitmapImageRep.FileType = .jpeg
+    public static var encodingFileType: NSBitmapImageRep.FileType = .jpeg
     
     ///Returns the data representation for the Image.
-    func data() -> Data? {
+    func data(for fileType: NSBitmapImageRep.FileTyp = encodingFileType) -> Data? {
         if let cgImage = cgImage(forProposedRect: nil, context: nil, hints: nil) {
             let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
-            if let data = bitmapRep.representation(using: Self.encodedFileType, properties: [:]) {
+            if let data = bitmapRep.representation(using: fileType, properties: [:]) {
                 return data
             }
         }
         
         return nil
     }
+}
+
+extension NSImage: CodableValueSupported {
+    public static let type = SupportedCodableTypes.image
     
     public func encode(to encoder: Encoder) throws {
         let imageData = data()
         var container = encoder.singleValueContainer()
         try container.encode(imageData)
     }
+    
 }
-
 #endif
