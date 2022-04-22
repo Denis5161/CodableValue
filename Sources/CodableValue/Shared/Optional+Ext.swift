20cb945c1509084462e98e0063b86bedaadbea98
Denis Goldberg <goldbergdenis00@gmail.com>
2022-04-23 00:47:27 +0200

    Update Optional+Ext.swift

--- Sources/CodableValue/Shared/Optional+Ext.swift
+++ Sources/CodableValue/Shared/Optional+Ext.swift
@@ -9,5 +9,5 @@ import Foundation
 
 //Add conformance to CodableValue for when the type is an optional.
 extension Optional: CodableValueSupported where Wrapped: CodableValueSupported {
-    public static var type: SupportedCodableTypes { Wrapped.type }
+    public static let type: SupportedCodableTypes { Wrapped.type }
 }
